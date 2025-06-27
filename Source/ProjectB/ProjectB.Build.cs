54bb9cebc0eaad96a0c2c692b7479de5641fb323
Northz <danhuyngoc@gmail.com>
2025-06-27 16:40:19 +0200

    Initial commit

---
+++ Source/ProjectB/ProjectB.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ProjectB : ModuleRules
+{
+	public ProjectB(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"ProjectB",
+			"ProjectB/Variant_Platforming",
+			"ProjectB/Variant_Combat",
+			"ProjectB/Variant_Combat/AI",
+			"ProjectB/Variant_SideScrolling",
+			"ProjectB/Variant_SideScrolling/Gameplay",
+			"ProjectB/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
