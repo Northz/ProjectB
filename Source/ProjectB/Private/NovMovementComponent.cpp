8b7603ad5b401e6b84e8dcc613fd056c52ea3448
Northz <danhuyngoc@gmail.com>
2025-06-27 18:59:29 +0200

    add helper for snapshotting CMC state data

--- Source/ProjectB/Private/NovMovementComponent.cpp
+++ Source/ProjectB/Private/NovMovementComponent.cpp
@@ -2,4 +2,48 @@
 
 
 #include "NovMovementComponent.h"
+#include "GameFramework/Character.h"
 
+bool UNovMovementComponent::FSavedMove_Nov::CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const
+{
+	FSavedMove_Nov* NewNovMove = static_cast<FSavedMove_Nov*>(NewMove.Get());
+
+	if (bSavedWantsToWalk != NewNovMove->bSavedWantsToWalk)
+	{
+		return false;
+	}
+	return FSavedMove_Character::CanCombineWith(NewMove, InCharacter, MaxDelta);
+}
+
+void UNovMovementComponent::FSavedMove_Nov::Clear()
+{
+	FSavedMove_Character::Clear();
+	bSavedWantsToWalk = 0;
+}
+
+uint8 UNovMovementComponent::FSavedMove_Nov::GetCompressedFlags() const
+{
+	uint8 Result = Super::GetCompressedFlags();
+
+	if (bSavedWantsToWalk) Result |= FLAG_Custom_0;
+
+	return Result;
+}
+
+void UNovMovementComponent::FSavedMove_Nov::SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData)
+{
+	FSavedMove_Character::SetMoveFor(Character, InDeltaTime, NewAccel, ClientData);
+
+	UNovMovementComponent* CharacterMovement = Cast<UNovMovementComponent>(Character->GetCharacterMovement());
+
+	bSavedWantsToWalk = CharacterMovement->bWantsToWalk;
+}
+
+void UNovMovementComponent::FSavedMove_Nov::PrepMoveFor(ACharacter* Character)
+{
+	Super::PrepMoveFor(Character);
+
+	UNovMovementComponent* CharacterMovement = Cast<UNovMovementComponent>(Character->GetCharacterMovement());
+
+	CharacterMovement->bWantsToWalk = bSavedWantsToWalk;
+}
--- Source/ProjectB/Public/NovMovementComponent.h
+++ Source/ProjectB/Public/NovMovementComponent.h
@@ -13,5 +13,42 @@ UCLASS()
 class PROJECTB_API UNovMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
-	
+
+	// Helper class, snapshots all state data in this movement component
+	// in order to reproduce a move for a given frame
+	class FSavedMove_Nov : public FSavedMove_Character
+	{
+		typedef FSavedMove_Character Super;
+
+		// Saved bWantsToWalk
+		uint8 bSavedWantsToWalk : 1;
+
+		// Checks current move and new move and tries to combine them
+		// i.e. when the saved data for two moves is the same
+		// saves bandwidth
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
+
+		// Resets the save move object to empty
+		virtual void Clear() override;
+
+		// Returns a byte (8-bit) containing special movement data
+		// to extend we must use assigned custom flags
+		// use for only essential data that is sent every single frame
+		// i.e. don't use for dashing, which should instead be handled by 
+		// an RPC (remote procedure call) to the server
+		virtual uint8 GetCompressedFlags() const override;
+
+		// Captures the state data of the CMC
+		virtual void SetMoveFor(
+			ACharacter* Character, 
+			float InDeltaTime, 
+			FVector const& NewAccel,
+			FNetworkPredictionData_Client_Character& ClientData) override;
+
+		// Takes the data from the set move and applies it to the current state
+		// basically the reverse of SetMoveFor.
+		virtual void PrepMoveFor(ACharacter* Character) override;
+	};
+
+	bool bWantsToWalk;
 };
